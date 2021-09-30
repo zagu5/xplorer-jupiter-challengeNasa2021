9e94c563a0d5384c10723de23e358c912202a61c
zagu5 <zury.agudelo@gmaol.com>
2021-09-30 11:23:39 -0500

    desarrollo nivel 1

---
+++ game.js
@@ -0,0 +1,230 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1.9,
+    debug: true,
+    clearColor: [0, 0, 0, 1]
+  })
+ 
+  //Speed identifiers
+  const MOVE_SPEED = 120
+  const JUMP_FORCE = 360
+  const BIG_JUMP_FORCE = 550
+  let CURRENT_JUMP_FORCE = JUMP_FORCE
+  const ENEMY_SPEED = 30
+  const FALL_DEATH = 400
+
+  // Game logic
+  let isJumping = true
+  
+  loadRoot('https://i.imgur.com/')
+  loadSprite('bg', 'D4GMuVE.png')
+  loadSprite('sample', 'v93pgWa.png')
+  loadSprite('sample1', 'NSsPkw0.png')
+  loadSprite('sample2', 'KoSieLh.png')
+  loadSprite('evil-tornado', 'zKC6rax.png')
+  loadSprite('brick', 'pogC9x5.png')
+  loadSprite('ground-jupiter', '2DrrRXW.png')
+  loadSprite('astronaut', 'tdwbmn9.png')
+  loadSprite('antidote', 'D5C793q.png')
+  loadSprite('bonus-nube', '5AuICN1.png')
+  loadSprite('unboxed', 'No8gXSw.png')
+  loadSprite('eddy', 'YEaSi0t.png')
+  loadSprite('eddy1', 'JMqD5NC.png')
+
+  scene ("game", ({ level, score })=> {
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    const map = [
+      '           °                                    ',
+      '           °                                    ',   
+      '           °                                    ',
+      '           °                                    ',
+      '          ===                                   ',
+      '                ===                             ',
+      '                           %  = %               ',
+      '                    %===                        ',  
+      '    *   % %                                |    ',
+      '                                          ==    ',
+      '                                        ==== +  ',
+      '          ^   -     ^     -    ^    - ======    ',
+      '================================================',
+    ]
+    
+    const levelCfg = {
+        width: 40,
+        height:30,
+        'x': [sprite('bg'), solid()],
+        '=': [sprite('ground-jupiter'), solid() ],
+        '$': [sprite('sample'), 'sample'],
+        '°': [sprite('sample1'), 'sample1'],
+        '|': [sprite('sample2'), 'sample2'],
+        '%': [sprite('bonus-nube'), solid(), 'sample-surprise'],
+        '*': [sprite('bonus-nube'), solid(), 'antidote-surprise'],
+        '}': [sprite('unboxed'), solid()],
+        '+': [sprite('eddy'), solid(), scale(0.7), 'eddy'],
+        '-': [sprite('eddy1'), solid(), scale(0.55), 'eddy1'],
+        '^': [sprite('evil-tornado'), solid(), 'dangerous'],
+        '#': [sprite('antidote'), solid(), 'antidote', body()],
+    }
+
+    add([
+      sprite('bg'),
+      layer('bg'),
+      pos(-305, 20),
+//     {width: width(100),height:height(100)}
+    ])
+
+    const gameLevel = addLevel(map, levelCfg)
+
+    const scoreLabel = add([
+      text(score),
+      pos(30,6),
+      layer('ui'),
+      {
+        value: score,
+      }
+    ])
+
+    scoreLabel.action(()=>{
+      camPos(player.pos)
+    })
+
+    add([
+      text('level' + parseInt(level + 1)), pos(40,6)
+    ])
+
+    function big(){
+      let timer = 0
+      let isBig = false
+      return {
+        update(){
+          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+          if (isBig){
+            timer -= dt()
+            if (timer <= 0) {
+              this.smallify()
+            }
+          }
+        },
+        isBig(){
+          return isBig
+        },
+        smallify(){
+          this.scale = vec2(1)
+          CURRENT_JUMP_FORCE = JUMP_FORCE
+          timer = 0
+          isBig = false
+        },
+        biggify(time){
+          this.scale = vec2(1.3)
+          timer = time
+          isBig = true
+        }
+        
+      }
+    }
+
+    const player = add([
+        sprite('astronaut'), 
+        solid(), 
+        pos(50,0),
+        body(),
+        big(),
+        origin('bot')        
+    ])
+
+    action('antidote', (a) => {
+      a.move(20,0)
+    })
+
+    player.on("headbump", (obj)=> {
+      if(obj.is('sample-surprise')){
+        gameLevel.spawn('$', obj.gridPos.sub(0,1))
+        destroy(obj)
+        gameLevel.spawn('}', obj.gridPos.sub(0,0))
+      }
+      if(obj.is('antidote-surprise')){
+        gameLevel.spawn('#', obj.gridPos.sub(0,1))
+        destroy(obj)
+        gameLevel.spawn('}', obj.gridPos.sub(0,0))
+      }
+    })
+
+    player.collides('antidote', (a) => {
+      destroy(a)
+      player.biggify(10)
+    })
+
+    player.collides('sample', (s) => {
+      destroy(s)
+      scoreLabel.value++
+      scoreLabel.text = scoreLabel.value
+    })
+
+    player.collides('sample1', (s) => {
+      destroy(s)
+      scoreLabel.value++
+      scoreLabel.text = scoreLabel.value
+    })
+
+    player.collides('sample2', (s) => {
+      destroy(s)
+      scoreLabel.value++
+      scoreLabel.text = scoreLabel.value
+    })
+
+    action('dangerous', (d) => {
+      d.move(-ENEMY_SPEED,0)
+    })
+
+    player.collides('dangerous', (d) => {
+      if(isJumping){
+        destroy(d)
+      }else {
+        go('lose', {score : scoreLabel.value})
+      }  
+    })
+
+    player.action(()=>{
+      camPos(player.pos)
+      if(player.pos.y >= FALL_DEATH){
+        go('lose', { score: scoreLabel.value})
+      }    })
+
+    player.collides('eddy',() => {
+      keyPress('down', () => {
+        go('game', {
+          level: (level + 1),
+          score: scoreLabel.value
+        })
+      })
+    })
+
+    keyDown('left', () =>{
+      player.move(-MOVE_SPEED,0)
+    })
+
+    keyDown('right', () =>{
+      player.move(MOVE_SPEED,0)
+    })
+
+    player.action(()=> {
+      if(player.grounded()){
+        isJumping = false
+      }
+    })
+
+    keyPress('space', () => {
+      if(player.grounded()){
+        isJumping = true
+        player.jump(CURRENT_JUMP_FORCE,0)
+      }
+    })
+
+  })
+
+  scene('lose', ({ score }) => {
+    add([text(score, 32), origin('center'), pos(width()/2, height()/2)])
+  })
+  start("game", { level:0, score:0 })
